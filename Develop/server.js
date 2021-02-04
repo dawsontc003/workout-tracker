b0401991834d132e3a82971aaec7c1bb8434ffbd
Thomas Dawson <dawsontc003@gmail.com>
2021-02-03 23:02:07 -0700

    Created service JS and initial api route

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongojs = require("mongojs");
+
+const app = express();
+
+const databaseUrl = "exercises";
+const collections = ["Workout"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", (error) => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
